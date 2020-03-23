4713eb554ffe6ecec24071f399494317ec176260
aivantg <aivant.developer@gmail.com>
2020-03-23 00:03:45 -0500

    fixed airtable login problem by using puppeteer instead of nightmare

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
-const path = require('path')
-const { writeJson } = require('fs-extra')
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
-
+const path = require('path');
+const { writeJson } = require('fs-extra');
+const puppeteer = require('puppeteer');
 
 function script() {
   return _.mapValues(application.tablesById, table =>
-    _.set(_.omit(table, ['sampleRows']), 'columns', _.map(table.columns, item =>
-      _.set(item, 'foreignTable', _.get(item, 'foreignTable.id')))))
+    _.set(
+      _.omit(table, ['sampleRows']),
+      'columns',
+      _.map(table.columns, item =>
+        _.set(item, 'foreignTable', _.get(item, 'foreignTable.id'))
+      )
+    )
+  );
 }
 
 function getUrl(baseId) {
-  return `https://airtable.com/login?continue=/${baseId}/api/docs`
+  return `https://airtable.com/login?continue=/${baseId}/api/docs`;
 }
 function getSavePath(baseId) {
-  return path.resolve(__dirname, 'downloads', `${baseId}.json`)
+  return path.resolve(__dirname, 'downloads', `${baseId}.json`);
 }
-module.exports = function ({ baseId, email, password }) {
-  nightmare
-    .goto(getUrl(baseId))
-    .wait(1000)
-    .wait('#sign-in-form-fields-root > div > label > div')
-    .type('#sign-in-form-fields-root > div > label > input[name="email"]', email)
-    .type('#sign-in-form-fields-root > div > label > input[name="password"]', password)
-    .click('#sign-in-form-fields-root > div > label > input[type="submit"]')
-    .wait(1000)
-    .wait('.docs > .languageTabs > .tab')
-    .evaluate(script)
-    .end()
-    .then(function (result) {
-      return writeJson(getSavePath(baseId), result)
-    })
-    .catch(function (error) {
-      console.error('Error:', error);
-    })
-}
+
+module.exports = async function({ baseId, email, password }) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(getUrl(baseId));
+  await page.waitForSelector('#sign-in-form-fields-root > div > label > div');
+  await page.type(
+    '#sign-in-form-fields-root > div > label > input[name="email"]',
+    email
+  );
+  await page.type(
+    '#sign-in-form-fields-root > div > label > input[name="password"]',
+    password
+  );
+  await page.click(
+    '#sign-in-form-fields-root > div > label > input[type="submit"]'
+  );
+
+  await page.waitForSelector('.docs > .languageTabs > .tab');
+  const result = await page.evaluate(script);
+  writeJson(getSavePath(baseId), result);
+  await browser.close();
+};
